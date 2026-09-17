95d4a62bb51fe5747652d6b9b1caf82534b60351
gongek <gongeksmail@gmail.com>
2026-09-17 12:17:53 +0200

    chore(config): add CDN images and redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dock-cdn.mrdn.online",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/pricing",
+        destination: "/premium",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
